d0e8e70c2fa648ddff594deb08cd36317d17c3bb
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-19 01:39:46 +0300

    Ders 19(Main/Admin Layout componentlerin oluşturulması)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeComponent } from './home/home.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from "./home/home.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { ContactComponent } from "./contact/contact.component";
+import { MainLayoutComponent } from "./layout/main-layout/main-layout.component";
+import { AdminLayoutComponent } from "./layout/admin-layout/admin-layout.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutMeComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ContactComponent,
+    MainLayoutComponent,
+    AdminLayoutComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
