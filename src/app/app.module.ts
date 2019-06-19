376d90d691e8b47c941de7f507893be4701c8e80
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-19 17:21:29 +0300

    Ders 29(App.module dosyamızı sadeleştirme)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,11 @@ import { MainModule } from "./pages/main.module";
 
 import { AppComponent } from "./app.component";
 
-import { HomeComponent } from "./pages/home/home.component";
-import { AboutMeComponent } from "./pages/about-me/about-me.component";
-import { ContactComponent } from "./pages/contact/contact.component";
-import { MainLayoutComponent } from "./layout/main-layout/main-layout.component";
-import { MainNavComponent } from "./nav/main-nav/main-nav.component";
-
 import { AdminLayoutComponent } from "./layout/admin-layout/admin-layout.component";
 import { AdminNavComponent } from "./nav/admin-nav/admin-nav.component";
 
-import { MenuCategoryComponent } from "./components/menu-category/menu-category.component"; ///bu  kaldırılacak.
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutMeComponent,
-    ContactComponent,
-    MainLayoutComponent,
-    AdminLayoutComponent,
-    MainNavComponent,
-    AdminNavComponent,
-    MenuCategoryComponent
-  ],
+  declarations: [AppComponent, AdminLayoutComponent, AdminNavComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, MainModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/main.module.ts
+++ src/app/pages/main.module.ts
@@ -9,9 +9,17 @@ import { ComponentsModule } from "../components/components.module";
 import { HomeComponent } from "./home/home.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { ContactComponent } from "./contact/contact.component";
+import { MainLayoutComponent } from "../layout/main-layout/main-layout.component";
+import { MainNavComponent } from "../nav/main-nav/main-nav.component";
 
 @NgModule({
-  declarations: [HomeComponent, AboutMeComponent, ContactComponent],
+  declarations: [
+    MainLayoutComponent,
+    MainNavComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ContactComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
