3d3fb944b312c6dd6e4c8fb5d014bdb40a8cb250
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-19 13:57:32 +0300

    Ders 24(CategoryService.ts dosyasının oluşturulması)

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+
+import { HttpClient } from "@angular/common/http";
+import { Category } from "../models/category";
+
+@Injectable({
+  providedIn: "root"
+})
+export class CategoryService {
+  private apiUrl: string = "https://localhost:44356/api/categories";
+  constructor(private httpCleint: HttpClient) {}
+
+  public getCategories() {
+    return this.httpCleint.get<Category[]>(this.apiUrl);
+  }
+
+  public getCategorybyId(id: number) {
+    let url = `${this.apiUrl}/${id}`;
+    return this.httpCleint.get<Category>(url);
+  }
+}
