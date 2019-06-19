066cfd66dd32234efc32000c2bc9ebef15c35e84
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-19 16:29:07 +0300

    Ders 26(Kategori menüsünün(menu-category component) html dosyasının kodlanması)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { ContactComponent } from "./contact/contact.component";
 import { MainLayoutComponent } from "./layout/main-layout/main-layout.component";
 import { AdminLayoutComponent } from "./layout/admin-layout/admin-layout.component";
-import { MainNavComponent } from './nav/main-nav/main-nav.component';
-import { AdminNavComponent } from './nav/admin-nav/admin-nav.component';
-import { MenuCategoryComponent } from './components/menu-category/menu-category.component';
+import { MainNavComponent } from "./nav/main-nav/main-nav.component";
+import { AdminNavComponent } from "./nav/admin-nav/admin-nav.component";
+import { MenuCategoryComponent } from "./components/menu-category/menu-category.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,7 @@ import { MenuCategoryComponent } from './components/menu-category/menu-category.
     AdminNavComponent,
     MenuCategoryComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
