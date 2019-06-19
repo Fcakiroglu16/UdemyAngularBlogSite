3cb563fb02138257f634d93aedd34fa5f6494a56
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-19 03:37:50 +0300

    Ders 22(Anasayfa header menü'de linkleri aktif yapma)

--- src/app/nav/main-nav/main-nav.component.ts
+++ src/app/nav/main-nav/main-nav.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+enum MainPage {
+  home = 1,
+  about_me = 2,
+  contact = 3
+}
 @Component({
-  selector: 'app-main-nav',
-  templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  selector: "app-main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrls: ["./main-nav.component.css"]
 })
 export class MainNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  pageActive: MainPage;
+  constructor(private router: Router) {
+    this.router.events.subscribe(x => {
+      if (x instanceof NavigationEnd) {
+        if (x.url.indexOf("anasayfa") > 0) {
+          this.pageActive = MainPage.home;
+        } else if (x.url.indexOf("hakkimda") > 0) {
+          this.pageActive = MainPage.about_me;
+        } else if (x.url.indexOf("iletisim") > 0) {
+          this.pageActive = MainPage.contact;
+        } else {
+          this.pageActive = MainPage.home;
+        }
+      }
+    });
   }
 
+  ngOnInit() {}
 }
