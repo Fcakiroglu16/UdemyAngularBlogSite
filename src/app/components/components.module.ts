2a7af7adea2d749436a9fa86f7203bb41701933d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-20 17:38:21 +0300

    Ders 35(sayfalama butonları için ngx-pagination kütüphanesinin projeye eklenmesi)

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { NgxPaginationModule } from "ngx-pagination";
+
 import { MenuCategoryComponent } from "./menu-category/menu-category.component";
 import { PageTitleComponent } from "./page-title/page-title.component";
 
 @NgModule({
   declarations: [MenuCategoryComponent, PageTitleComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxPaginationModule],
   exports: [MenuCategoryComponent, PageTitleComponent]
 })
 export class ComponentsModule {}
