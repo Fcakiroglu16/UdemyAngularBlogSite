e2569dc4e011644942893a515fcab108b1c1ba41
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-21 04:11:22 +0300

    Ders 43(farklı sayfalara geçişlerde devam eden ajax isteklerini iptal etme)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Article } from "src/app/models/article";
 import { ArticleService } from "src/app/services/article.service";
 import { Router, ActivatedRoute } from "@angular/router";
@@ -7,12 +7,16 @@ import { Router, ActivatedRoute } from "@angular/router";
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    if (this.ajax != null) this.ajax.unsubscribe();
+  }
   page: number = 1;
   articles: Article[];
   totalCount: number;
   pageSize: number = 5;
   loadingItem: number = 5;
+  ajax;
   constructor(
     private articleService: ArticleService,
     private router: Router,
@@ -36,7 +40,7 @@ export class HomeComponent implements OnInit {
       this.articles = [];
       this.totalCount = 0;
 
-      this.articleService
+      this.ajax = this.articleService
         .getArticles(this.page, this.pageSize)
         .subscribe(data => {
           console.log(data);
