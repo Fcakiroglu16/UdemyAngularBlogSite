9879daf88a30a425d611609e01f3369d83f6cc1e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-25 22:10:29 +0300

    Ders 70(article-add(makale ekleme) component'inin kodlanması-2)

--- src/app/admin-pages/article/article-add/article-add.component.ts
+++ src/app/admin-pages/article/article-add/article-add.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ArticleService } from "src/app/services/article.service";
 
 @Component({
-  selector: 'app-article-add',
-  templateUrl: './article-add.component.html',
-  styleUrls: ['./article-add.component.css']
+  selector: "app-article-add",
+  templateUrl: "./article-add.component.html",
+  styleUrls: ["./article-add.component.css"]
 })
 export class ArticleAddComponent implements OnInit {
+  fileData: File = null;
+  picture: string = null;
+  constructor(private articleService: ArticleService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  upload(files) {
+    this.fileData = files.target.files[0];
+
+    let formData = new FormData();
+
+    formData.append("picture", this.fileData);
+
+    this.articleService.saveArticlePicture(formData).subscribe(result => {
+      console.log(result.path);
+      this.picture = result.path;
+    });
   }
-
 }
