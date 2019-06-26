3d2ae4e5769f2cb497c145d568e4c583dc2b9150
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-27 02:01:56 +0300

    Ders 82(admin-login component'in oluşturulması-1)

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  IsAuthenticated(email: string, password: string) {
+    let adminUser = { email: email, password: password };
+
+    return this.httpClient.post<any>(
+      "https://localhost:44356/api/Auth/IsAuthenticated",
+      adminUser
+    );
+  }
+}
